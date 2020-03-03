f066297860fb2b902aceacd7c2c1308bb5a6e65b
iykekings <iykekings36@gmail.com>
2020-03-03 12:56:31 +0100

    scaffold app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,13 +23,13 @@ describe('AppComponent', () => {
   it(`should have as title 'shotterweb'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shotterweb');
+    expect(app.title).toEqual('shotter');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('shotterweb app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('shotter app is running!');
   });
 });
